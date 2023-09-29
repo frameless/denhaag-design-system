ab1ec6d7051ad1dab7f368d30ee86680ab378a9f
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-09-29 17:56:05 +0200

    fix: merge conflicts are resolved

---
+++ .storybook/stories/templates/Lopende-zaken-page.stories.tsx
@@ -0,0 +1,125 @@
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { Page, PageHeader, PageFooter } from '../../../components/Page/src';
+import { HeaderLogic, HeaderLogicProps } from '../../../components/Header/src';
+import Footer from '../../../components/Footer/src';
+import ResponsiveContent from '../../../components/ResponsiveContent/src';
+import { InboxIcon, ArchiveIcon, UserIcon, ParkingIcon, CircleInformationIcon } from '../../../components/Icons/src';
+import { Heading2 } from '../../../components/Typography/src';
+import { CaseCard } from '../../../components/Card/src';
+import { Tabs } from '../../../components/Tab/src';
+import {
+  headerProps,
+  footerLegalData,
+  footerSocialData,
+  newsletterData,
+  contactData,
+  copyright,
+} from '../templates/util';
+
+import './template-story.scss';
+import { Sidenav, SidenavItem, SidenavLink, SidenavList } from '../../../components/Sidenav/src';
+
+const meta = {
+  title: 'Templates/LopendeZaken',
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+  },
+} as Meta<typeof Page>;
+
+export default meta;
+
+const zakenHeaderProps: HeaderLogicProps = {
+  ...headerProps,
+  breadcrumbs: {
+    navigationPath: [
+      { label: 'Home', href: 'https://denhaag.nl/' },
+      { label: 'MijnDenHaag', href: 'https://denhaag.nl/' },
+      { label: 'Mijn lopende zaken' },
+    ],
+  },
+};
+
+const tabsProps = {
+  tabData: [
+    { label: 'Lopende Zaken', panelContent: '' },
+    { label: 'Afgeronde zaken', panelContent: '' },
+  ],
+};
+
+type Story = StoryObj<typeof meta>;
+
+export const LopendeZaken: Story = {
+  render: (args) => (
+    <Page {...args}>
+      <PageHeader>
+        <HeaderLogic {...zakenHeaderProps} />
+      </PageHeader>
+      <ResponsiveContent className="denhaag-page-content denhaag-responsive-content--sidebar">
+        <Sidenav>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink current={true}>
+                <InboxIcon />
+                label
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink>
+                <ArchiveIcon />
+                Mijn lopende zaken
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink>
+                <CircleInformationIcon />
+                Belastingszaken
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink>
+                <CircleInformationIcon />
+                WOZ
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink>
+                <ParkingIcon />
+                Parkeren
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink>
+                <CircleInformationIcon />
+                Erfpacht
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink>
+                <UserIcon />
+                Mijn gegevens
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+        </Sidenav>
+        <main className="denhaag-page-content__main">
+          <Heading2>Mijn lopende zaken</Heading2>
+          <Tabs {...tabsProps}></Tabs>
+          <section className="denhaag-card-group">
+            <CaseCard date={new Date('2020-01-21T00:00:00.000Z')} title="Aanvraag subsidie geluidsisolatie" />
+            <CaseCard date={new Date('2020-01-21T00:00:00.000Z')} title="Aanvraag Ooievaarspas" />
+          </section>
+        </main>
+      </ResponsiveContent>
+      <PageFooter>
+        <Footer
+          newsletterData={newsletterData}
+          contactData={contactData}
+          legalData={footerLegalData}
+          copyrightLabel={copyright}
+          socialData={footerSocialData}
+        />
+      </PageFooter>
+    </Page>
+  ),
+};
