9a1b7795993bd705d466a9b935f46dc918aa0b5d
savitris <11764984+savitris@users.noreply.github.com>
2023-09-29 16:00:48 +0200

    fix: added Actions

--- .storybook/stories/templates/02-Overview-page.stories.tsx
+++ .storybook/stories/templates/02-Overview-page.stories.tsx
@@ -1,30 +1,39 @@
 import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { Heading2, Paragraph } from '../../../components/Typography/src';
+import { Heading2, Heading3, Paragraph } from '../../../components/Typography/src';
 import { Page, PageHeader, PageFooter } from '../../../components/Page/src';
 import { HeaderLogic, HeaderLogicProps } from '../../../components/Header/src';
 import Footer from '../../../components/Footer/src';
 import ResponsiveContent from '../../../components/ResponsiveContent/src';
 import { Sidenav, SidenavItem, SidenavLink, SidenavList } from '../../../components/Sidenav/src';
-import { ArchiveIcon, GridIcon, InboxIcon, UserIcon, ArrowRightIcon, HouseIcon, ParkingIcon, AlertTriangleIcon } from '../../../components/Icons/src';
+import {
+  ArchiveIcon,
+  GridIcon,
+  InboxIcon,
+  UserIcon,
+  ArrowRightIcon,
+  HouseIcon,
+  ParkingIcon,
+  // AlertTriangleIcon,
+} from '../../../components/Icons/src';
 import { copyright, headerProps, footerLegalData, footerSocialData, newsletterData, contactData } from './util';
+import { Link } from '../../../components/Link/src';
 
 import './template-story.scss';
 
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableHeader,
-} from '../../../components/Table/src';
+// import {
+//   Table,
+//   TableBody,
+//   TableCaption,
+//   TableHead,
+//   TableRow,
+//   TableCell,
+//   TableHeader,
+// } from '../../../components/Table/src';
 
 import { CaseCard } from '../../../components/Card/src';
 
-
-
+import { Action } from '../../../components/Action/src';
 
 const meta = {
   title: 'Templates/OverviewPage',
@@ -42,6 +51,8 @@ const overviewHeaderProps: HeaderLogicProps = {
   },
 };
 type Story = StoryObj<typeof meta>;
+
+let deadlineDate = new Date(new Date().setDate(new Date().getDate() + 2));
 
 export const OverviewPage: Story = {
   render: () => (
@@ -65,7 +76,7 @@ export const OverviewPage: Story = {
               </SidenavLink>
             </SidenavItem>
             <SidenavItem>
-              <SidenavLink >
+              <SidenavLink>
                 <GridIcon />
                 Belastingzaken
               </SidenavLink>
@@ -83,13 +94,13 @@ export const OverviewPage: Story = {
               </SidenavLink>
             </SidenavItem>
             <SidenavItem>
-              <SidenavLink >
+              <SidenavLink>
                 <GridIcon />
                 Erfpacht
               </SidenavLink>
             </SidenavItem>
             <SidenavItem>
-              <SidenavLink >
+              <SidenavLink>
                 <UserIcon />
                 Mijn gegevens
               </SidenavLink>
@@ -97,51 +108,65 @@ export const OverviewPage: Story = {
           </SidenavList>
         </Sidenav>
         <main className="denhaag-page-content__main">
-          <Heading2>Hallo, Anne Klap!</Heading2>
+          <Heading2>Hallo Anne</Heading2>
           <Paragraph>
-In MijnDenHaag kunt u zelf uw persoonlijke zaken regelen wanneer het u uitkomt. U kunt bijvoorbeeld uw rekeningen betalen en zien wanneer uw aanvraag klaar is.
+            In MijnDenHaag kunt u zelf uw persoonlijke zaken regelen wanneer het u uitkomt. U kunt bijvoorbeeld uw
+            rekeningen betalen en zien wanneer uw aanvraag klaar is.
           </Paragraph>
+
+          <Heading3>Wat ik moet regelen</Heading3>
+          {/* Must not be a series of Action components, use another component */}
+          <Action link="#example" deadline={deadlineDate} relativeDate>
+            Geef informatie voor uw aanvraag subsidie geluidisolatie
+          </Action>
+
+          <Action link="#example" deadline={deadlineDate}>
+            Betaal uw parkeerbon van € 74,90 voor parkeren bij Valeriusplein
+          </Action>
+
+          <Action link="#example">Verleng uw identiteitskaart</Action>
+
+          {/* <br />
+          <br />
           <Table>
             <TableCaption>Wat ik moet regelen</TableCaption>
             <TableHead>
               <TableRow>
                 <TableCell />
                 <TableHeader className="heman">He-Man</TableHeader>
-
               </TableRow>
             </TableHead>
             <TableBody>
               <TableRow>
                 <TableHeader scope="row">Geef informatie voor uw aanvraag subsidie geluidisolatie</TableHeader>
                 <TableCell>
-                   <AlertTriangleIcon></AlertTriangleIcon>
-
-                   nog 2 dagen!! <ArrowRightIcon color="inherit"></ArrowRightIcon></TableCell>
-
+                  <AlertTriangleIcon></AlertTriangleIcon>
+                  nog 2 dagen!! <ArrowRightIcon color="inherit"></ArrowRightIcon>
+                </TableCell>
               </TableRow>
               <TableRow>
                 <TableHeader scope="row">Betaal uw parkeerbon van € 74,90 voor parkeren bij Valeriusplein </TableHeader>
-                <TableCell>vóór 1 maart 2023<ArrowRightIcon color="inherit"></ArrowRightIcon></TableCell>
+                <TableCell>
+                  vóór 1 maart 2023<ArrowRightIcon color="inherit"></ArrowRightIcon>
+                </TableCell>
               </TableRow>
               <TableRow>
                 <TableHeader scope="row">Verleng uw identiteitskaart</TableHeader>
-                <TableCell><ArrowRightIcon color="inherit"></ArrowRightIcon></TableCell>
-
+                <TableCell>
+                  <ArrowRightIcon color="inherit"></ArrowRightIcon>
+                </TableCell>
               </TableRow>
             </TableBody>
-          </Table>
-          <CaseCard
-  date={new Date("2020-10-17T00:00:00.000Z")}
-  href="#"
-  title="Aanvraag subsidie geluidsisolatie"
+          </Table> */}
+          <Heading3>Mijn lopende zaken</Heading3>
+          <Paragraph>
+            <Link href="#" icon={<ArrowRightIcon />}>
+              Bekijk alle zaken
+            </Link>
+          </Paragraph>
 
-/>
-<CaseCard
-  date={new Date("2020-08-01T00:00:00.000Z")}
-  href="#"
-  title="Aanvraag Ooievaarspas"
-/>
-
+          <CaseCard date={new Date('2020-10-17T00:00:00.000Z')} href="#" title="Aanvraag subsidie geluidsisolatie" />
+          <CaseCard date={new Date('2020-08-01T00:00:00.000Z')} href="#" title="Aanvraag Ooievaarspas" />
         </main>
       </ResponsiveContent>
       <PageFooter>
